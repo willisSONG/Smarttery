09059921f31c5223d9898f13762dd9a5a8df2ff3
WillisSONG <songkai3908032@163.com>
2015-11-15 23:03:05 +0800

    remove static

--- smarttery-static/Smarttery-commons/src/main/java/com/song/lottery/constant/LotteryConstant.java
+++
@@ -1,9 +0,0 @@
-package com.song.lottery.constant;
-
-public class LotteryConstant {
-
-	/**
-	 * 此字段用于滤出抓取文本中的乱码
-	 */
-	public static final String FILTER_GARBLED = "[^\\w^\\u4E00-\\u9FFF^/^\\uff1a^:^\\^=^,^<^>^;^&^\\.^!^\\(^\\)^\"^\\{^\\}^\\$^#^\\s^“^”^-^,^\\?^\\[^\\]^\\-^_^+^'^，^。^%^、^；,^（^）^\\u2014]";
-}
